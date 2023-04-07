8ff2bd1ce9176b672abb9937a9513beb8c984d68
pinky1812 <tratzar-x@yahoo.com>
2023-04-07 16:48:43 -0400

    With timestamp

--- Robot/capture.py
+++ Robot/capture.py
@@ -9,7 +9,7 @@ import numpy as np
 epoch = datetime.utcfromtimestamp(0)
 
 def unix_time_millis(dt):
-    return (dt - epoch).total_seconds() * 1000.0
+    return round((dt - epoch).total_seconds() * 1000.0,0)
 
 # Camera settings
 cam_width = 1280  # Cam sensor width settings
@@ -44,6 +44,6 @@ if (os.path.isdir("../photos") == False):
 for frame in camera.capture_continuous(capture, format="rgba", use_video_port=True, resize=(img_width, img_height)):
     counter += 1
     filename = '../photos/photo_' + str(img_width) + 'x' + str(img_height) + '_' + \
-               unix_time_millis(datetime.now()) + '.jpg'
+               str(unix_time_millis(datetime.now())) + '.jpg'
     cv2.imwrite(filename, frame)
     time.sleep(0.05)
