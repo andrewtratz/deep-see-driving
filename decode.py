81f6f9bca053731cd27174264f8a0c77770c5f30
pinky1812 <tratzar-x@yahoo.com>
2023-04-18 21:16:15 -0400

    Perspective cameras

--- decode.py
+++ decode.py
@@ -13,24 +13,34 @@ import math
 import cv2
 
 # Focal length
-# fl = 1.8
-fl = 1.5
+fl = 1.15
+
 # Define field of view
-fov = 80
+fov = 70
+
+# x_axis_rotation = math.radians(0)
+# y_axis_rotation = math.radians(0)
+
+y_axis_rotation = math.radians(-3)
+x_axis_rotation = math.radians(-10)
+
+x_translation = 0.35
+y_translation = 0.0
+z_translation = 0
 
 # Define coordinates of principal point
-px = 0.05
-py = 0.1
+px = 0
+py = 0
 
 crop_bottom = 0
 
 # Load camera image
-cam = cv2.imread("rectified_sample_photo.jpg")
-# cam = cv2.rotate(cam, cv2.ROTATE_90_CLOCKWISE)
-# cam = cv2.rotate(cam, cv2.ROTATE_90_CLOCKWISE)
-# cam = cv2.cvtColor(cam, cv2.COLOR_BGR2RGB)
+cam = cv2.imread("cups_photo.jpg")
+cam = cv2.rotate(cam, cv2.ROTATE_90_CLOCKWISE)
+cam = cv2.rotate(cam, cv2.ROTATE_90_CLOCKWISE)
+cam = cv2.cvtColor(cam, cv2.COLOR_BGR2RGB)
 cam = cv2.cvtColor(cam, cv2.COLOR_BGR2GRAY)
-cam = cam // 3
+cam = cam // 10
 cam = cv2.cvtColor(cam, cv2.COLOR_GRAY2RGB)
 
 # Extract left image only
@@ -50,9 +60,7 @@ rpm = 600
 calibration_file = r'Puck Hi-Res.yml'
 
 # Data input
-pcap_file = r'velo2.pcap'
-
-
+pcap_file = r'velo.pcap'
 
 # Read first frame of the pcap
 config = vd.Config(model=model_type, rpm=rpm, calibration_file=calibration_file)
@@ -61,12 +69,15 @@ config.max_angle = fov/2
 pcap_file = pcap_file
 cloud_arrays = []
 
+i = 0
 for stamp, points in vd.read_pcap(pcap_file, config):
+    i += 1
     cloud_arrays.append(points)
-    break # Only get first set of data points
+    if i == 10:
+        break # Only get first set of data points
 
 # Add distance as a column to the dataset
-data = np.array(cloud_arrays[0])
+data = np.array(cloud_arrays[i-1])
 dist = np.sqrt(data[:,0]**2 + data[:,1]**2 + data[:,2]**2)
 data = np.hstack((data, np.expand_dims(dist, 1)))
 
@@ -77,7 +88,20 @@ data = data[data[:,0] >= 0.0]
 newdata = np.hstack([np.expand_dims(dim, 1) for dim in (-data[:,2], -data[:,1], data[:,0], data[:,6])])
 
 
-# od = 1280 # Output image dimensions
+translation_matrix = np.array([[1, 0, 0, x_translation],
+                               [0, 1, 0, y_translation],
+                               [0, 0, 1, z_translation],
+                               [0, 0, 0, 1]])
+
+y_axis_rotation_matrix = np.array([[1, 0, 0, 0],
+                                   [0, math.cos(y_axis_rotation), -math.sin(y_axis_rotation), 0],
+                                   [0, math.sin(y_axis_rotation), math.cos(y_axis_rotation), 0],
+                                   [0, 0, 0, 1]])
+
+x_axis_rotation_matrix = np.array([[math.cos(x_axis_rotation), 0, math.sin(x_axis_rotation), 0],
+                                   [0, 1, 0, 0],
+                                   [0, -math.sin(x_axis_rotation), math.cos(x_axis_rotation), 0],
+                                   [0, 0, 0, 1]])
 
 # Camera matrix
 C = np.array([[fl,0,py,0],
@@ -89,11 +113,19 @@ vec = newdata[:,0:3].T
 t = np.ones((1, vec.shape[1]))
 vec = np.vstack((vec, t))
 
-# Try to project into fisheye space
-# fisheye = vec.copy()
+# Rotation
+vec = np.matmul(x_axis_rotation_matrix, vec)
+vec = np.matmul(y_axis_rotation_matrix, vec)
+
+# Translation
+vec = np.matmul(translation_matrix, vec)
+
 
 # Project into camera space
-projected = np.matmul(C, vec).T
+projected = np.matmul(C, vec)
+
+# Transpose
+projected = projected.T
 
 # Add the distance information back to the projected points
 projected = np.hstack((projected, np.expand_dims(newdata[:,3], 1)))
@@ -111,12 +143,15 @@ for i in range(projected.shape[0]):
     row = projected[i]
     dist = row[3]
 
-    if dist >= 0.75:
+    max_clip = 2.5
+    min_clip = 0.75
+
+    if dist >= max_clip:
         color = 1
-    elif dist <= 0.5:
+    elif dist <= min_clip:
         color = 0
     else:
-        color = (dist - 0.5) / 0.25
+        color = (dist - min_clip) / (max_clip - min_clip)
 
     if row[0] > 0 and row[0] < h and row[1] > 0 and row[1] < w:
         img[int(row[0]), int(row[1]), 0] = (1 - color) * 255
